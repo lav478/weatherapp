aa9e665b009903951b6deeed23a0076624ea9a75
lav478 <konkayala11@gmail.com>
2024-11-01 15:37:36 +0530

    Update app.py

--- weather/app.py
+++ weather/app.py
@@ -51,4 +51,4 @@ def predict():
     return render_template('index.html', prediction=result, unique_values=unique_values)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
